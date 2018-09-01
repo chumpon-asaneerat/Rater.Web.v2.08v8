95d7ca7c004e28b43cd37057350a959965ebec97
chumpon-asaneerat <chumponsenate@gmail.com>
2018-09-01 20:42:39 +0700

    Implement ExpressServer wrapper.

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log('server.js running...');
+// base modules require.
+const path = require('path');
+
+// setup root path.
+process.env['ROOT_PATHS'] = path.dirname(require.main.filename);
+const rootPath = process.env['ROOT_PATHS'];
+// server modules require.
+//const appcfg = require(path.join(rootPath, 'lib', 'app-config'));
+const expSvr = require(path.join(rootPath, 'lib', 'express-server'));
+
+//console.log(appcfg);
+const server = new expSvr.ExpressServer();
+
+// start server.
+server.start();
+
